8253825116e3e767ad0827a0345dc7b1ac8d5c8a
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-06-14 21:23:20 +0530

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "ESIIT",
-  description: "ESIIT homepage",
+  description: "ESIIT homepage pre alpha made for demonstrational purposes.",
   icons: {
     icon: "./favicon.png", // public path
   },
