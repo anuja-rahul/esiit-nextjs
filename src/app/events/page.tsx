3f9c878458d1f0202d9d31ef169b66b555ff885b
Hans <khansikaseneviratne@gmail.com>
2024-06-20 02:30:20 +0530

    created hero slider
    todo:(need to move inline styles to a separate file)

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,3 +1,4 @@
+import Carousel from "@/components/events/eventSlider/Carousel";
 import "./index.scss";
 
 export const metadata = {
@@ -8,9 +9,7 @@ export default function Events() {
   return (
     <>
       <main className="flex min-h-screen items-center justify-center bg-transparent">
-        <h1 className="font-['Extatica'] text-2xl font-extrabold">
-          This is the Events page.
-        </h1>
+        <Carousel />
       </main>
     </>
   );
